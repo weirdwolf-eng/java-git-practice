399ad2884de3784d2efdcc309c95c2647160dc07
Saksham Verma <sakshamverma2205@gmail.com>
2026-02-13 09:57:24 +0530

    Updated HelloGit output message

--- helloGit.java
+++ helloGit.java
@@ -1,6 +1,6 @@
 public class helloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
-        System.out.println("My IDE is connected to Git successfully.");
+        System.out.println("Hello GitHub!");
+        System.out.println("First successful Git push 🚀");
     }
 }
