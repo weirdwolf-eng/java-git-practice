65c5781d2931743f1d5e425cc960274630abe3db
Saksham Verma <sakshamverma2205@gmail.com>
2026-02-27 08:53:14 +0530

    UseCase9

---
+++ UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+public class UseCase9PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        String input = "madam";
+        boolean isPalindrome = check(input, 0, input.length() - 1);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+
+    private static boolean check(String s, int start, int end) {
+        if (start >= end) return true;
+        if (s.charAt(start) != s.charAt(end)) return false;
+        return check(s, start + 1, end - 1);
+    }
+}
