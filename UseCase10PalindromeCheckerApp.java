2a1e97120fb3e000dd17437cd5c6b6c54b06b04c
Saksham Verma <sakshamverma2205@gmail.com>
2026-02-27 08:59:49 +0530

    UseCase10

---
+++ UseCase10PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+public class UseCase10PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String input = "A man a plan a canal Panama";
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < normalized.length() / 2; i++) {
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
