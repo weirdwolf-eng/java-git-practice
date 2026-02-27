233eb5f2b0e78a9f06a66992058bc32b3b7a93a0
Saksham Verma <sakshamverma2205@gmail.com>
2026-02-27 08:38:32 +0530

    UC6palindrome

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String input = "civic";
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
